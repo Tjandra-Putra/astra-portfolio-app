d434a7b239e93bf5a1d51a3c69879dc2639a9447
Tjandra Putra <tjandrap.work@gmail.com>
2023-12-21 15:40:01 +0800

    navbar update sticky

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -8,9 +8,9 @@ import { Badge } from "@/components/ui/badge";
 // default route for the app "https://localhost:3000/"
 export default function Home() {
   return (
-    <div className="min-h-screen h-full shadow-paper bg-white rounded-lg mt-4 p-5">
+    <div className="min-h-screen h-full shadow-paper bg-white rounded-lg mt-4 p-6">
       <div className="flex justify-between">
-        <div className="job-title font-medium">Software Engineer</div>
+        <div className="job-title font-normal">Software Engineer</div>
         <div className="status uppercase">
           <Badge variant={"success"}>Available for work</Badge>
         </div>
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,81 +1,73 @@
 "use client";
 
 import { useState } from "react";
-import { ClerkProvider, SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { SignedIn, UserButton } from "@clerk/nextjs";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faCircleDot,
-  faMessage,
-  faAddressCard,
-  faUser,
-  faEnvelope,
-  faPaperPlane,
-  faFolder,
-} from "@fortawesome/free-regular-svg-icons";
+import { faAddressCard, faEnvelope, faFolder, faPenToSquare } from "@fortawesome/free-regular-svg-icons";
 import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faAt } from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
+import { Button } from "./ui/button";
 
 const Navbar = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(true);
 
   return (
-    <nav className="sticky h-16 shadow-paper bg-white rounded-lg flex flex-row justify-between items-center p-5">
-      <div className="nav-left flex gap-7">
+    <nav className="sticky top-[1rem] h-16 shadow-paper bg-white rounded-lg flex flex-row justify-between items-center py-5 px-2">
+      <div className="nav-left flex gap-0">
         <Link href="/" className="nav-item">
-          <FontAwesomeIcon
-            icon={faFolder}
-            className="w-6 h-6 transition duration-300 hover:text-black"
-            color="#9b9ca5"
-          />
+          <Button variant="ghost">
+            <FontAwesomeIcon icon={faFolder} className="w-6 h-6 transition duration-300" color="#9b9ca5" />
+          </Button>
         </Link>
         <Link href="/about" className="nav-item">
-          <FontAwesomeIcon
-            icon={faAddressCard}
-            className="w-6 h-6 transition duration-300 hover:text-black"
-            color="#9b9ca5"
-          />
+          <Button variant="ghost">
+            <FontAwesomeIcon icon={faAddressCard} className="w-6 h-6 transition duration-300" color="#9b9ca5" />
+          </Button>
         </Link>
         <Link href="/contact" className="nav-item">
-          <FontAwesomeIcon
-            icon={faEnvelope}
-            className="w-6 h-6 transition duration-300 hover:text-black"
-            color="#9b9ca5"
-          />
+          <Button variant="ghost">
+            <FontAwesomeIcon icon={faEnvelope} className="w-6 h-6 transition duration-300" color="#9b9ca5" />
+          </Button>
         </Link>
       </div>
-      <div className="nav-right flex gap-7">
+      <div className="nav-right flex gap-0">
         <Link href="/" className="nav-item">
-          <FontAwesomeIcon icon={faAt} className="w-6 h-6 transition duration-300 hover:text-black" color="#9b9ca5" />
+          <Button variant="ghost">
+            <FontAwesomeIcon icon={faAt} className="w-6 h-6 transition duration-300" color="#9b9ca5" />
+          </Button>
         </Link>
         <Link href="/" className="nav-item">
-          <FontAwesomeIcon
-            icon={faGithub}
-            className="w-6 h-6 transition duration-300 hover:text-black"
-            color="#9b9ca5"
-          />
+          <Button variant="ghost">
+            <FontAwesomeIcon icon={faGithub} className="w-6 h-6 transition duration-300" color="#9b9ca5" />
+          </Button>
         </Link>
         <Link href="/" className="nav-item">
-          <FontAwesomeIcon
-            icon={faLinkedin}
-            className="w-6 h-6 transition duration-300 hover:text-black"
-            color="#9b9ca5"
-          />
+          <Button variant="ghost">
+            <FontAwesomeIcon icon={faLinkedin} className="w-6 h-6 transition duration-300" color="#9b9ca5" />
+          </Button>
         </Link>
-        <div className="nav-item">
-          <SignedIn>
-            <UserButton
-              appearance={{
-                elements: {
-                  avatarBox: {
-                    width: "1.65rem",
-                    height: "1.65rem",
+        <SignedIn>
+          <Link href="/" className="nav-item">
+            <Button variant="ghost">
+              <FontAwesomeIcon icon={faPenToSquare} className="w-6 h-6 transition duration-300" color="#9b9ca5" />
+            </Button>
+          </Link>
+          <div className="nav-item">
+            <Button variant="ghost">
+              <UserButton
+                appearance={{
+                  elements: {
+                    avatarBox: {
+                      width: "1.65rem",
+                      height: "1.65rem",
+                    },
                   },
-                },
-              }}
-            />
-          </SignedIn>
-        </div>
+                }}
+              />
+            </Button>
+          </div>
+        </SignedIn>
       </div>
     </nav>
   );
