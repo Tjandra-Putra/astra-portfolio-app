127be33a0fc35bfb3d7b18795d11d23d08c98fa8
Tjandra Putra <tjandrap.work@gmail.com>
2023-12-23 16:31:43 +0800

    code refactor project detailed page

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,11 +1,21 @@
+import React from "react";
 import { faArrowRight, faCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Button } from "./ui/button";
 import ProjectCard from "./project-card";
 
-const Projects = ({ title }) => {
+interface Project {
+  id: number;
+  title: string;
+}
+
+interface ProjectsProps {
+  title?: string;
+}
+
+const Projects: React.FC<ProjectsProps> = ({ title }) => {
   // Dummy data for projects (replace it with your actual data)
-  const projectsData = [
+  const projectsData: Project[] = [
     { id: 1, title: "Project 1" },
     { id: 2, title: "Project 2" },
     { id: 3, title: "Project 3" },
@@ -13,14 +23,14 @@ const Projects = ({ title }) => {
   ];
 
   // Slice the array to show only the first 3 items
-  const visibleProjects = projectsData.slice(0, 3);
+  const visibleProjects: Project[] = projectsData.slice(0, 3);
 
   return (
     <div className="projects bg-ash p-6 rounded-lg">
       <div className="flex justify-between">
         <div className="flex items-center gap-2">
           <FontAwesomeIcon icon={faCircle} className="w-2 h-2" color="#9b9ca5" />
-          <div className="font-medium text-gray-800 text-lg">{title ? title : "Projects"}</div>
+          <div className="font-medium text-gray-800 text-lg">{title || "Projects"}</div>
         </div>
         <Button variant="white">
           View All <FontAwesomeIcon icon={faArrowRight} className="ms-2" color="#000000" />
