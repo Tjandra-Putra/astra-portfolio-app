abee649798412b48f6c004f7d1a5a946987128e2
Tjandra Putra <tjandrap.work@gmail.com>
2023-12-30 21:17:59 +0800

    profile page data render

--- app/(main)/(routes)/profile/[username]/page.tsx
+++ app/(main)/(routes)/profile/[username]/page.tsx
@@ -7,16 +7,21 @@ import { faFile, faCopy } from "@fortawesome/free-regular-svg-icons";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import Projects from "@/components/projects";
 import Experiences from "@/components/experiences";
+import { currentProfile } from "@/lib/current-profile";
 
 // default route for the app "https://localhost:3000/"
-export default function Home() {
+export default async function Home() {
+  const profile = await currentProfile();
+
   return (
     <div className="">
       <section className="introduction pb-5">
         <div className="flex justify-between mb-5">
           <div className="flex items-center gap-2 mb-4">
             <FontAwesomeIcon icon={faCircle} className="w-2 h-2" color="#9b9ca5" />
-            <div className="job-title font-medium text-gray-800 text-lg">Software Engineer</div>
+            <div className="job-title font-medium text-gray-800 text-lg">
+              {profile?.jobTitle ? profile.jobTitle : "[Job Title]"}
+            </div>
           </div>
           <Link href="/contact" className="status uppercase tracking-wider text-end">
             <Badge variant={"diamond"} className="font-semibold">
@@ -30,12 +35,10 @@ export default function Home() {
           <div className="md:col-span-8">
             <div className="w-full">
               <div className="name text-4xl font-medium">
-                Hello, I'm <span className="text-primary">Tjandra</span>
+                Hi, I'm{" "}
+                <span className="text-primary">{profile?.name ? profile.name.split(" ")[0] : "[First Name]"}</span>
               </div>
-              <div className="description mt-3 text-gray-900 font-normal">
-                I'm passionate about building products that make a positive impact on people's lives. apple apple apple
-                apple apple apple apple.
-              </div>
+              <div className="description mt-3 text-gray-900 font-normal">{profile?.bio ? profile.bio : "[Bio]"}</div>
               <div className="buttons mt-5 space-x-3">
                 <Button variant={"navy"}>
                   <FontAwesomeIcon icon={faFile} className="me-2" color="#ffffff" />
@@ -48,12 +51,16 @@ export default function Home() {
               </div>
             </div>
           </div>
-          <div className="md:col-span-4">
+          <div className="md:col-span-4 flex items-center">
             <div className="w-full">
               <div className="avatar-border border-4 border-[#000000] p-2 rounded-full">
                 <Avatar>
-                  <AvatarImage src="https://github.com/shadcn.png" />
-                  <AvatarFallback>CN</AvatarFallback>
+                  {profile?.imageUrl ? (
+                    <AvatarImage src={profile.imageUrl} />
+                  ) : (
+                    <AvatarImage src="https://github.com/shadcn.png" />
+                  )}
+                  <AvatarFallback>{profile?.name}</AvatarFallback>
                 </Avatar>
               </div>
             </div>
