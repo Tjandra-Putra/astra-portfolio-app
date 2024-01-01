fb4001fe924bd21e667e44c0ba5984d9e0435697
Tjandra Putra <tjandrap.work@gmail.com>
2024-01-01 23:39:27 +0800

    loader for detail project page

--- app/(main)/(routes)/projects/[id]/page.tsx
+++ app/(main)/(routes)/projects/[id]/page.tsx
@@ -8,6 +8,7 @@ import ReactMarkdown from "react-markdown";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowAltCircleRight } from "@fortawesome/free-regular-svg-icons";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import Image from "next/image";
 
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
@@ -45,43 +46,63 @@ const Project = () => {
   return (
     <React.Fragment>
       <section className="bg-ash rounded-lg p-6">
-        <div className="company">
-          <div className="flex mb-3">
-            <div className="text-[#1c3454] w-36">Company</div>
-            <div className="text-black w-full">{project?.company}</div>
-          </div>
-        </div>
-
-        <div className="project-title">
-          <div className="flex mb-3">
-            <div className="text-[#1c3454] w-36">Title</div>
-            <div className="text-black w-full">{project?.name}</div>
-          </div>
-        </div>
-
-        <div className="project-type">
-          <div className="flex mb-3">
-            <div className="text-[#1c3454] w-36">Category</div>
-            <div className="text-black w-full">{project?.category}</div>
-          </div>
-        </div>
-
-        <div className="start-date">
-          <div className="flex">
-            <div className="text-[#1c3454] w-36">Date</div>
-            <div className="text-black w-full">
-              {project?.startDate ? `${formatDate(project.startDate)} to ${formatDate(project.endDate)}` : "N/A"}{" "}
+        {!project ? (
+          "Loading..."
+        ) : (
+          <>
+            <div className="company">
+              <div className="flex mb-3">
+                <div className="text-[#1c3454] w-36">Company</div>
+                <div className="text-black w-full">{project?.company}</div>
+              </div>
             </div>
-          </div>
-        </div>
+
+            <div className="project-title">
+              <div className="flex mb-3">
+                <div className="text-[#1c3454] w-36">Title</div>
+                <div className="text-black w-full">{project?.name}</div>
+              </div>
+            </div>
+
+            <div className="project-type">
+              <div className="flex mb-3">
+                <div className="text-[#1c3454] w-36">Category</div>
+                <div className="text-black w-full">{project?.category}</div>
+              </div>
+            </div>
+
+            <div className="start-date">
+              <div className="flex">
+                <div className="text-[#1c3454] w-36">Date</div>
+                <div className="text-black w-full">
+                  {project?.startDate ? `${formatDate(project.startDate)} to ${formatDate(project.endDate)}` : "N/A"}{" "}
+                </div>
+              </div>
+            </div>
+          </>
+        )}
       </section>
 
       <section>
-        <img
-          src={project?.thumbnailUrl}
-          alt={project?.name}
-          className="thumbnail-img w-full h-full rounded-lg my-6 shadow-paper"
-        />
+        {!project ? (
+          <div className="h-full w-full my-6 rounded-lg bg-ash p-6">Loading...</div>
+        ) : project?.thumbnailUrl ? (
+          <Image
+            src={project?.thumbnailUrl}
+            alt={project?.name}
+            width={500}
+            height={500}
+            className="thumbnail-img w-full h-full rounded-lg my-6 shadow-paper"
+          />
+        ) : (
+          <Image
+            src="/assets/image/pexels-fauxels-3183186.jpg"
+            alt={project?.name}
+            width={500}
+            height={500}
+            className="thumbnail-img w-full h-full rounded-lg my-6 shadow-paper"
+          />
+        )}
         <div className="title text-2xl font-semibold capitalize">{project?.name}</div>
         <div className="description capitalize my-3 leading-6">{project?.description}</div>
         <div className="buttons flex flex-row justify-end">
