97640bdacf7ef8fdae8a51b329908a30c3aae568
Tjandra Putra <tjandrap.work@gmail.com>
2024-01-05 20:17:25 +0800

    react spinners loader

--- components/layout/loader.tsx
+++ components/layout/loader.tsx
@@ -1,7 +1,12 @@
+import BarLoader from "react-spinners/BarLoader";
+import BeatLoader from "react-spinners/BeatLoader";
+import ClipLoader from "react-spinners/ClipLoader";
+
 const Loader = () => {
   return (
     <div className="projects bg-ash p-6 my-6 rounded-lg w-full text-center flex justify-center items-center">
-      <p>Loading...</p>
+      {/* <p>Loading...</p> */}
+      <BarLoader loading={true} color="#1d3455" />
     </div>
   );
 };
