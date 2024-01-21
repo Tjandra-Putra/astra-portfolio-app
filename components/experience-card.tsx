46aa6a34ed63b19cea4470d40ee9ecd80c47b33c
Tjandra Putra <tjandrap.work@gmail.com>
2024-01-21 21:29:32 +0800

    profile page ui update

--- components/experience-card.tsx
+++ components/experience-card.tsx
@@ -13,8 +13,8 @@ const ExperienceCard: React.FC<ProjectCardComponentProps> = ({ data }) => {
     <div className="bg-white rounded-lg my-5 p-3 flex items-center justify-between ring-offset-background transition-all duration-300 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 hover:ring-[3px] hover:ring-[#74c0fc] disabled:pointer-events-none disabled:opacity-50">
       <div className="flex items-center gap-4">
         <div className="avatar-border border-2 border-[#f0f1f3] p-2 rounded-full">
-          <div className="avatar h-14 w-14 flex items-center justify-center bg-[#183153] text-[#eecd4e] font-bold rounded-full capitalize">
-            {data.name[0]}
+          <div className="avatar h-14 w-14 flex items-center justify-center bg-[#183153] text-[#ffffff] font-semibold rounded-full capitalize">
+            {data.workExperienceTitle[0]}
           </div>
         </div>
         <div className="flex flex-col">
