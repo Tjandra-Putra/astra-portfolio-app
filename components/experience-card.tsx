643c951d66d2a641dfd59efe4172dd8ec5603e13
Tjandra Putra <tjandrap.work@gmail.com>
2024-02-03 15:33:30 +0800

    project/experience card mobile responsiveness

--- components/experience-card.tsx
+++ components/experience-card.tsx
@@ -13,7 +13,7 @@ const ExperienceCard: React.FC<ProjectCardComponentProps> = ({ data }) => {
     <div className="bg-white rounded-lg my-5 p-3 flex items-center justify-between ring-offset-background transition-all duration-300 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 hover:ring-[3px] hover:ring-[#74c0fc] disabled:pointer-events-none disabled:opacity-50">
       <div className="flex items-center gap-4">
         <div className="avatar-border border-2 border-[#f0f1f3] p-2 rounded-full">
-          <div className="avatar h-14 w-14 flex items-center justify-center bg-[#183153] text-[#ffffff] font-semibold rounded-full capitalize">
+          <div className="avatar sm:h-14 sm:w-14 h-10 w-10 flex items-center justify-center bg-[#183153] text-[#ffffff] font-semibold rounded-full capitalize">
             {data.workExperienceTitle[0]}
           </div>
         </div>
