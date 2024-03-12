fc0467980f50999922a03b72f5ac72966d955e06
Tjandra Putra <tjandrap.work@gmail.com>
2024-03-12 13:01:07 +0800

    footer padding fixed

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,10 +1,13 @@
 const Footer = () => {
   return (
-    <div className="bg-navy mb-6 mt-3 p-6 text-center rounded-lg">
+    <div className="bg-navy sm:mb-0 mb-3 sm:mt-6 mt-3 p-6 text-center rounded-lg">
       <div className="text-sm font-normal text-white">
         <span className="text-white">© 2023 Astra - Portfolio Platform </span>
         <br />
-        Developed by Tjandra
+        Developed by{" "}
+        <a href="https://www.linkedin.com/in/tjandra-putra/" target="_blank">
+          Tjandra
+        </a>
       </div>
     </div>
   );
