9124416ead6e1680d02111f60df1d0e4a144134f
Tjandra Putra <tjandrap.work@gmail.com>
2024-04-27 21:04:30 +0800

    certificate public page

--- app/(main)/(routes)/certificate/page.tsx
+++ app/(main)/(routes)/certificate/page.tsx
@@ -1,15 +1,39 @@
 "use client";
 
+import React, { useEffect } from "react";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { faAward, faCertificate, faChevronRight, faCircle, faLink } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
-import React from "react";
+import axios from "axios";
+import Loader from "@/components/layout/loader";
 
 const CertificatePage = () => {
-  return (
+  const [certificates, setCertificates] = React.useState<any[]>([]);
+  const [loading, setLoading] = React.useState(false);
+
+  const fetchCertificates = async () => {
+    try {
+      setLoading(true);
+
+      const response = await axios.get("/api/manage/certificate");
+      setCertificates(response.data);
+    } catch (error) {
+      console.error("Error fetching certificates:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchCertificates();
+  }, []);
+
+  return loading ? (
+    <Loader />
+  ) : (
     <React.Fragment>
       <div className="flex items-center gap-2 mb-3">
         <FontAwesomeIcon icon={faCircle} className="w-2 h-2" color="#9b9ca5" />
@@ -21,36 +45,44 @@ const CertificatePage = () => {
       </div>
 
       <div className="bg-ash sm:px-6 sm:py-[0.1rem] py-[0.1rem] px-3 rounded-lg">
-        <div className="certificate-container bg-white rounded-lg sm:p-6 p-3 sm:my-6 my-3">
-          <div className="header grid sm:grid-cols-[3fr,9fr] grid-cols-[3fr,9fr] gap-4 items-center">
-            <div>
-              <div className="sm:w-[100px] sm:h-[100px] w-[70px] h-[70px] object-cover rounded-lg bg-navy flex items-center justify-center">
-                <FontAwesomeIcon icon={faAward} className="text-white w-10 h-10" />
+        {certificates?.map((certificate) => (
+          <div className="certificate-container bg-white rounded-lg sm:p-6 p-3 sm:my-6 my-3" key={certificate.id}>
+            <div className="header grid sm:grid-cols-[3fr,9fr] grid-cols-[3fr,9fr] gap-4 items-center">
+              <div>
+                <div className="sm:w-[100px] sm:h-[100px] w-[70px] h-[70px] object-cover rounded-lg bg-navy flex items-center justify-center">
+                  <FontAwesomeIcon icon={faAward} className="text-white w-10 h-10" />
+                </div>
+              </div>
+              <div className="flex flex-col">
+                <div className="certification-title font-semibold text-xs sm:text-base leading-4">
+                  {certificate.title}
+                </div>
+                <div className="issuer text-gray-600 font-medium text-xs sm:text-base sm:mt-0 mt-1">Microsoft</div>
+                <div className="duration text-gray-500 font-normal text-xs sm:text-base">Issued Jul 2023</div>
               </div>
             </div>
-            <div className="flex flex-col">
-              <div className="certification-title font-semibold text-xs sm:text-base leading-4">
-                Microsoft Azure Fundamentals (AZ-900)
-              </div>
-              <div className="issuer text-gray-600 font-medium text-xs sm:text-base sm:mt-0 mt-1">Microsoft</div>
-              <div className="duration text-gray-500 font-normal text-xs sm:text-base">Issued Jul 2023</div>
+
+            <Separator className="sm:mt-6 mt-3" />
+
+            <img
+              src={certificate.certificateImageUrl}
+              alt="certificate"
+              className="w-full sm:mt-6 mt-3 shadow-paper rounded-lg"
+            />
+
+            <div className="certificate-id flex justify-end">
+              <Badge variant={"sky"} className="text-xs text-gray-900 font-medium sm:mt-6 mt-3 text-end">
+                Certificate ID: {certificate.certificateId}
+              </Badge>
             </div>
+
+            <Link href={certificate.certificateUrl} target="_blank">
+              <Button variant={"navy"} className="w-full sm:mt-6 mt-3 font-medium">
+                View Certificate <FontAwesomeIcon icon={faLink} className="ml-2" />
+              </Button>
+            </Link>
           </div>
-
-          <Separator className="sm:mt-6 mt-3" />
-
-          <img
-            src="https://miro.medium.com/v2/resize:fit:1128/1*EWttqrihTanNGjRlorbpZg.png"
-            alt="certificate"
-            className="w-full sm:mt-6 mt-3 shadow-paper rounded-lg"
-          />
-
-          <Link href="/certificate/1" target="_blank">
-            <Button variant={"navy"} className="w-full sm:mt-6 mt-3">
-              View Certificate <FontAwesomeIcon icon={faLink} className="ml-2" />
-            </Button>
-          </Link>
-        </div>
+        ))}
       </div>
     </React.Fragment>
   );
