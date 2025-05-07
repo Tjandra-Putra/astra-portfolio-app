f8f2f5d33ae2b6b5c3f7767e06dddf1d701d295e
Tjandra Putra <tjandrap.work@gmail.com>
2025-05-07 17:20:42 +0800

    dark mode for home page

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -85,9 +85,9 @@ const LandingPage = () => {
   console.log("Profiles:", profiles);
 
   return (
-    <div className="flex flex-col items-center p-3 bg-ash min-h-screen h-full">
+    <div className="flex flex-col items-center p-3 bg-ash min-h-screen h-full dark:bg-[#0c0c0c] dark:border dark:border-white/10">
       <div className="w-full sm:w-[570px]">
-        <div className="min-h-[100vh] h-full shadow-paper bg-white rounded-xl sm:p-6 p-3">
+        <div className="min-h-[100vh] h-full shadow-paper bg-white rounded-xl sm:p-6 p-3 dark:bg-[#171717] dark:border dark: border-[#333335]">
           <Badge variant="sky" className="text-base flex justify-between w-full rounded-lg sm:mb-6 mb-3 h-15 p-6 hover:bg-[#74c0fc]">
             <div>
               <FontAwesomeIcon icon={faRocket} className="w-4 h-4 me-2" color="#183153" />
@@ -98,7 +98,7 @@ const LandingPage = () => {
               <Link href="/sign-in">
                 <Button variant="secondary" className="rounded-full">
                   Sign In
-                  <FontAwesomeIcon icon={faArrowRightToBracket} className="w-4 h-4 ms-2" color="#000000" />
+                  <FontAwesomeIcon icon={faArrowRightToBracket} className="w-4 h-4 ms-2 dark:text-white" />
                 </Button>
               </Link>
             </SignedOut>
@@ -122,11 +122,11 @@ const LandingPage = () => {
 
           <div className="shadow-paper sm:p-6 p-3 sm:mb-6 mb-3 rounded-lg">
             <div className="flex flex-col justify-center">
-              <div className="sm:text-3xl text-xl text-center font-semibold text-[#1d3554]">
+              <div className="sm:text-3xl text-xl text-center font-semibold text-[#1d3554] dark:text-zinc-300">
                 Discover inspiration and connect with exceptional <span className="text-[#fab306]">talent.</span>
               </div>
 
-              <div className="sm:text-base text-sm text-center text-[#1d3554] sm:mt-6 mt-3 sm:mb-8 mb-5">
+              <div className="sm:text-base text-sm text-center text-[#1d3554] sm:mt-6 mt-3 sm:mb-8 mb-5 dark:text-zinc-400">
                 <span className="font-semibold">Astra Portfolio</span> offers a platform to showcase your projects, experiences, and skills, enabling
                 connections with like-minded individuals and sharing your journey globally.
               </div>
@@ -147,11 +147,11 @@ const LandingPage = () => {
             </div>
           </div>
 
-          <div className="bg-ash shadow-paper sm:p-6 p-3 rounded-lg">
+          <div className="bg-ash shadow-paper sm:p-6 p-3 rounded-lg dark:bg-[#0c0c0c] dark:border dark:border-white/10">
             <div className="flex justify-between">
               <div className="flex items-center gap-2">
                 <FontAwesomeIcon icon={faCircle} className="w-2 h-2 text-[#1d3554]" />
-                <div className="font-medium text-gray-800 sm:text-lg text-base">Profiles</div>
+                <div className="font-medium text-gray-800 sm:text-lg text-base dark:text-zinc-300">Profiles</div>
               </div>
             </div>
 
@@ -162,7 +162,10 @@ const LandingPage = () => {
               filteredProfiles
                 // .filter((profile) => !profile.userId.includes("login_pending_user"))
                 .map((profile) => (
-                  <div className="individual-container rounded-lg bg-white sm:p-6 p-6 sm:mt-6 mt-3" key={profile.id}>
+                  <div
+                    className="individual-container rounded-lg bg-white sm:p-6 p-6 sm:mt-6 mt-3 dark:bg-[#171717] dark:border dark: border-[#333335]"
+                    key={profile.id}
+                  >
                     <div className="flex sm:flex-row flex-col justify-between items-center sm:text-start text-center">
                       <div className="flex sm:flex-row flex-col items-center sm:gap-6 gap-3">
                         <div className="">
@@ -177,8 +180,8 @@ const LandingPage = () => {
                         </div>
 
                         <div className="flex flex-col">
-                          <div className="font-semibold text-base text-[#1d3554]">{profile?.name}</div>
-                          <div className="text-base text-gray-500">{profile.jobTitle}</div>
+                          <div className="font-semibold text-base text-[#1d3554] dark:text-zinc-300">{profile?.name}</div>
+                          <div className="text-base text-gray-500 dark:text-zinc-400">{profile.jobTitle}</div>
                         </div>
                       </div>
                       <Link href={`/profile/${profile.id}`}>
