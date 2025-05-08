a6178ec56314b842873be14555d3e448a4c03afc
Tjandra Putra <tjandrap.work@gmail.com>
2025-05-08 20:56:45 +0800

    production routing redirect

--- next.config.js
+++ next.config.js
@@ -4,6 +4,21 @@ const nextConfig = {
   images: {
     domains: ["utfs.io", "vutz38vdur.ufs.sh"],
   },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        has: [
+          {
+            type: "host",
+            value: "tjandra.xyz",
+          },
+        ],
+        destination: "/profile/35c89c6b-98ab-487f-a295-959a18090bc6",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
