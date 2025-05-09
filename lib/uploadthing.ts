c801236946a7f057599580300dcc736f40ec5e11
Tjandra Putra <tjandrap.work@gmail.com>
2025-05-09 17:01:12 +0800

    attempt file upload in editor

--- lib/uploadthing.ts
+++ lib/uploadthing.ts
@@ -1,5 +1,7 @@
 import { generateComponents } from "@uploadthing/react";
+import { generateReactHelpers } from "@uploadthing/react/hooks";
 
 import type { OurFileRouter } from "@/app/api/uploadthing/core";
 
+export const { uploadFiles } = generateReactHelpers<OurFileRouter>();
 export const { UploadButton, UploadDropzone, Uploader } = generateComponents<OurFileRouter>();
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,8 @@
 import type { Config } from "tailwindcss";
+import { withUt } from "uploadthing/tw";
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./pages/**/*.{js,ts,jsx,tsx,mdx}", "./components/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       backgroundImage: {
@@ -16,4 +13,4 @@ const config: Config = {
   },
   plugins: [],
 };
-export default config;
+export default withUt(config);
