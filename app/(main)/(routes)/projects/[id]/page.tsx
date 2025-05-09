8759a060299577e717ad8485a5e20550213904ee
Tjandra Putra <tjandrap.work@gmail.com>
2025-05-10 00:41:38 +0800

    attempt fix duplication

--- app/(main)/(routes)/projects/[id]/page.tsx
+++ app/(main)/(routes)/projects/[id]/page.tsx
@@ -186,7 +186,7 @@ const Project = () => {
 
         <Separator className="mt-6 mb-6" />
 
-        {project?.content?.length > 351 ? <Editor initialContent={project?.content} editable={false} /> : null}
+        <Editor initialContent={project?.content} editable={false} />
 
         <div className="badges flex flex-row flex-wrap gap-3 my-6">
           {project.tags
